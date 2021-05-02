72037ad7e797ed7864cc1d565a0113f58bf26021
yonghwan-shin <39160195+yonghwan-shin@users.noreply.github.com>
2021-05-02 17:29:55 +0900

    upload arduino codes

---
+++ GP2Y0A21_IR_distance/GP2Y0A21_IR_distance.ino
@@ -0,0 +1,17 @@
+int IRpin = 32;
+int sensor_val;
+float dist;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensor_val = map(analogRead(IRpin), 0, 4096, 0, 3300);
+  dist = (24.61 / (sensor_val - 0.1696)) * 1000;
+  Serial.print(dist);
+  Serial.println(" cm");
+  delay(100);
+}
