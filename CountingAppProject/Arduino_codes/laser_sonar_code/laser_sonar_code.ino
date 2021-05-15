568496d2f7dcd90739e844bc4ffafcabdb9167d7
junwon-wj <69384594+junwon-wj@users.noreply.github.com>
2021-05-15 12:59:16 +0900

    add laser+sonar code
    코드 추가함

--- CountingAppProject/Arduino_codes/laser_sonar_code/laser_sonar_code.ino
+++ CountingAppProject/Arduino_codes/laser_sonar_code/laser_sonar_code.ino
@@ -1,3 +1,6 @@
+int IRpin = 34; // Put sensor_value pin to 32
+int sensor_val;
+float dist;
 #define echoPin 32 // attach pin 32 Arduino to pin Echo of HC-SR04
 #define trigPin 33 //attach pin 33 Arduino to pin Trig of HC-SR04
 
@@ -6,28 +9,36 @@
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
 
+
+
+
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  // put your setup code here, to run once:
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  Serial.begin(115200); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
+  Serial.begin(115200);
 }
+
 void loop() {
-  // Clears the trigPin condition
-  digitalWrite(trigPin, LOW);
+  // put your main code here, to run repeatedly:
+digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distance = duration * 0.0343 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.print("Distanc  e: ");
+  Serial.print("Sonar Distance: ");
   Serial.print(distance);
+  Serial.print(" cm");
+
+
+
+
+  sensor_val = map(analogRead(IRpin), 0, 4096, 0, 3300);
+  dist = (24.61 / (sensor_val - 0.1696)) * 1000;
+  Serial.print("  laser ");
+  Serial.print(dist);
   Serial.println(" cm");
-//  delay(100);
 }
